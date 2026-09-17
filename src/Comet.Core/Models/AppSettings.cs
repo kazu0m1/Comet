9a9ca7739652567868697e7052521b4539252b3e
kazu0m1 <65547812+kazu0m1@users.noreply.github.com>
2026-09-18 08:35:35 +0900

    Enable thumbnail sidebar by default

--- src/Comet.Core/Models/AppSettings.cs
+++ src/Comet.Core/Models/AppSettings.cs
@@ -2,11 +2,12 @@ namespace Comet.Core.Models;
 
 public sealed record AppSettings
 {
+    public int SettingsSchemaVersion { get; init; } = 1;
     public FitMode FitMode { get; init; } = FitMode.BestFit;
     public ReadingDirection ReadingDirection { get; init; } = ReadingDirection.RightToLeft;
     public PageLayoutMode PageLayoutMode { get; init; } = PageLayoutMode.DoublePage;
     public bool StretchSmallImages { get; init; } = true;
-    public bool ShowThumbnails { get; init; } = false;
+    public bool ShowThumbnails { get; init; } = true;
     public double ThumbnailWidth { get; init; } = 180;
     public bool FlipPageAtScrollEdge { get; init; } = true;
     public double SmartScrollFraction { get; init; } = 0.78;
